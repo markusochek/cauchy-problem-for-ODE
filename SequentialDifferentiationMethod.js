5991ebb05f1dd173421ca6a15c5cbbfc066583b9
Sagalaev Mark <sagalaev.mark@gmail.com>
2023-05-09 21:38:21 +0400

    final

--- SequentialDifferentiationMethod.js
+++ SequentialDifferentiationMethod.js
@@ -1,40 +1,31 @@
-export function sequentialDifferentiationMethod() {
-    // let x = 0;
-    // let y = 1/2;
-    // let yDerivative = `y^2 + x^2`;
-    // let yDerivative2ByX = math.derivative(yDerivative, `x`).toString();
-    // let yDerivative2ByY = math.derivative(yDerivative, 'y').toString();
-    // let yDerivativeEvaluate = math.evaluate(yDerivative, {y, x});
-    // let yDerivativeEvaluate2 = math.evaluate(yDerivative2ByY, {y, x}) * yDerivative +
-    //                    math.evaluate(yDerivative2ByX, {y, x});
-    // let yDerivative3 =
-    // console.log(yDerivativeEvaluate);
-    // console.log(yDerivativeEvaluate2);
-
-    derivative();
+export function sequentialDifferentiationMethod(m, l) {
+    let y = 0;
+    let x = 0;
+    let Derivatives = [];
+    Derivatives.push(derivative1(m, l, y, x));
+    Derivatives.push(derivative2(m, l, y, x, Derivatives[0]));
+    Derivatives.push(derivative3(m, l, y, x, Derivatives[0], Derivatives[1]));
+    Derivatives.push(derivative4(m, l, y, x, Derivatives[0], Derivatives[1], Derivatives[2]));
+    return Derivatives;
 }
 
-function derivative() {
-    let yDerivative = `y^2+x^2`;
-    let str = "";
-    for (let i = 0; i < yDerivative.length; i++) {
-        if (yDerivative[i] === "^") {
-            if (str !== "") {
-                str += " + "
-            }
-            str += "2 * ";
-            switch(String(yDerivative[i+1] - 1)) {
-                case "0":
-                    str += "1";
-                    break;
-                case "1":
-                    str += yDerivative[i-1];
-                    break;
-                default:
-                    str += yDerivative[i-1] + "^" + String(yDerivative[i+1] - 1);
-                    break;
-            }
-        }
-    }
-    console.log(str)
+function derivative1(m, l, y, x) {
+    return 1 + m * y * Math.sin(x) - l * y**2;
+}
+
+function derivative2(m, l, y, x, Derivative1) {
+    return m * Math.sin(x) * Derivative1 - 2 * l * Derivative1 * y + m * y * Math.cos(x);
+}
+
+function derivative3(m, l, y, x, Derivative1, Derivative2) {
+    return m * Math.cos(x) * Derivative1 - m * y * Math.sin(x) + m * Math.sin(x) * Derivative2 - 2 * l * Derivative2 * y
+           + m * Derivative1 * Math.cos(x) - 2 * l * Derivative1**2;
+
+}
+
+function derivative4(m, l, y, x, Derivative1, Derivative2, Derivative3) {
+    return -m * Math.sin(x) * Derivative1 - m * y * Math.cos(x) + m * Math.cos(x) * Derivative2
+           - m * Derivative1 * Math.sin(x) - 4 * l * Derivative2 * Derivative1 + m * Math.cos(x) * x * Derivative2
+           - m * Derivative1 * Math.sin(x) + m * Math.sin(x) * Derivative3 - 2 * l * Derivative3 * y
+           - 2 * l * Derivative2 * Derivative1 + m * Derivative2 * Math.cos(x);
 }
